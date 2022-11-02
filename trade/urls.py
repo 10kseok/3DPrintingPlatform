3b9c65b7be5410d96c0c2ead5fec076a714dd1e4
danhyeon <sodanhyun@gmail.com>
2022-11-02 13:30:39 +0900

    수정

--- trade/urls.py
+++ trade/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.estimate),
-    path('', views.bid),
-    path('', views.trade),
+    path('', views.index),
 ]
--- trade/views.py
+++ trade/views.py
@@ -2,18 +2,9 @@ from django.shortcuts import render
 from .models import Estimate, Bid, Trade
 
 
-def estimate(request):
+def index(request):
     estimate = Estimate.objects.filter(project_name='짱구')
-    context = {'estimate': estimate}
-    return render(request, 'trade/34_구매거래완료.html', context)
-
-def bid(request):
     bid = Bid.objects.filter(finished=False)
-    context = {'bid': bid}
-    return render(request, 'trade/34_구매거래완료.html', context)
-
-def trade(request):
     trade = Trade.objects.filter(product_state=1)
-    context = {'trade': trade}
-    return render(request, 'trade/34_구매거래완료.html', context)
-# Create your views here.
+    context = {'estimate': estimate, 'bid': bid, 'trade': trade}
+    return render(request, 'trade/35_판매거래완료.html', context)
