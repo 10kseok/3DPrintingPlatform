907069252e42c9e773b36456d355106a3b00f587
csw8820 <84608524+csw8820@users.noreply.github.com>
2022-11-09 18:19:50 +0900

    거래완료페이지
    구매자, 판매자 모두 거래완료 url연결해두었습니다.

--- trade/urls.py
+++ trade/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('buy/complete/', views.buy_complete, name='buy_complete'),
+    path('sell/complete/', views.sell_complete, name='sell_complete'),
 ]
